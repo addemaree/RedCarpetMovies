4e3b76c4c100b09f6d2bdd5946c58b56b4527e94
Adam Demaree <demareeadam1@gmail.com>
2017-05-07 09:31:33 -0400

    Add project files.

---
+++ RedCarpetMovies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RedCarpetMovies.MvcApplication" Language="C#" %>
---
+++ RedCarpetMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedCarpetMovies.Startup))]
+namespace RedCarpetMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

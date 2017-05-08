8aa3d770c9fdfb6e4256d96eb1b49e48c3af590a
Adam Demaree <demareeadam1@gmail.com>
2017-05-08 13:53:56 -0400

    Added Web API

---
+++ RedCarpetMovies/Dtos/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using RedCarpetMovies.Models;
+
+namespace RedCarpetMovies.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Your name is too long")]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        
+        public byte MembershipTypeId { get; set; }
+
+        
+       // [Min18YearsIfAMember]
+        public DateTime? Birthdaydate { get; set; }
+    }
+}
